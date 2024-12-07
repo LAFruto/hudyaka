2035f0b061221d20bc3cbcac80d9055a27a217be
Luis Angelo Fruto <luisfruto7@gmail.com>
2024-12-07 16:30:44 +0800

    fix: og

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -9,15 +9,40 @@ import { getActivitiesByType } from "~/models";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "Hudyaka - Mapúa MCM Website" },
+    { title: "Hudyaka - Mapúa MCM Foundation Week Website" },
     {
       name: "Mapúa MCM 9th Foundation - Website",
       content:
         "Hudyaka Mapúa: Kasadya ug pasalamat sa Padayong Pag-uswag. December 8 - 14, 2024 at Mapúa Malayan Colleges Mindanao",
     },
     {
+      property: "og:title",
+      content: "Hudyaka - Mapúa MCM Website",
+    },
+    {
+      property: "og:description",
+      content:
+        "Hudyaka Mapúa: Kasadya ug pasalamat sa Padayong Pag-uswag. December 8 - 14, 2024 at Mapúa Malayan Colleges Mindanao",
+    },
+    {
+      property: "og:type",
+      content: "website",
+    },
+    {
+      property: "og:url",
+      content: "https://hudyaka-mmcm.vercel.app",
+    },
+    {
       property: "og:image",
       content: "https://hudyaka-mmcm.vercel.app/social-preview.png",
+    },
+    {
+      property: "og:image:width",
+      content: "1200",
+    },
+    {
+      property: "og:image:height",
+      content: "630",
     },
   ];
 };
