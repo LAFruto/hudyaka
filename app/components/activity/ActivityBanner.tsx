ca6f7cb50cc42ca28fd3102da88d971092cd2aae
Luis Angelo Fruto <luisfruto7@gmail.com>
2024-12-08 14:24:14 +0800

    feat: time range

--- app/components/activity/ActivityBanner.tsx
+++ app/components/activity/ActivityBanner.tsx
@@ -7,6 +7,7 @@ import { Link } from "@remix-run/react";
 import { getEventStatus } from "~/lib/util";
 import { CountdownTimer } from "../CountdownTimer";
 import { useCurrentTime } from "~/hooks/useCurrentTime";
+import Clock from "../icons/clock";
 
 interface ActivityBannerProps {
   activity: ActivityRecord;
@@ -72,16 +73,31 @@ const ActivityBanner = ({ activity }: ActivityBannerProps) => {
                 {status.message}
               </p>
             ) : status.type === "countdown" ? (
-              <p className="inline-flex w-full justify-center rounded-lg items-center gap-2 text-white lg:px-4 py-1.5 text-lg md:text-xl font-semibold mt-4">
+              <div className="flex flex-col w-full justify-center rounded-lg items-center gap-2 text-white lg:px-4 py-1.5 font-semibold mt-4">
                 <CountdownTimer
                   currentTime={currentTime}
                   timeUntilStart={status.timeUntilStart}
+                  className="text-lg md:text-xl"
                 />
-              </p>
+                <p className="inline-flex items-center bg-red-800  text-white rounded-md px-4 py-1">
+                  <Clock className="w-4 h-4 mr-2" />
+                  {status.timeRange}
+                </p>
+              </div>
+            ) : status.type === "upcoming" ? (
+              <div className="flex flex-col w-full justify-center rounded-lg items-center gap-2 text-white lg:px-4 py-1.5 font-semibold mt-4">
+                <p className="text-lg lg:text-xl">{status.message}</p>
+                <p className="inline-flex items-center bg-red-800 text-white rounded-md px-4 py-1">
+                  <Clock className="w-4 h-4 mr-2" />
+                  {status.timeRange}
+                </p>
+              </div>
             ) : (
-              <p className="inline-flex w-full justify-center rounded-lg items-center gap-2 text-white lg:px-4 py-1.5 text-lg md:text-xl font-semibold mt-4">
-                {status.message}
-              </p>
+              <div className="flex items-center justify-center">
+                <p className="inline-flex   rounded-lg items-center gap-2 bg-red-800 text-white lg:px-4 py-1.5 text-lg md:text-xl font-semibold mt-4 ">
+                  {status.message}
+                </p>
+              </div>
             )}
           </div>
         </div>
