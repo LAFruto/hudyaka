6e2cebbcb1447e1ebf10901510681211ba96f367
Kakarot <adriancarlomenguita@gmail.com>
2024-12-09 22:35:24 +0800

    fix: kysely connection string

--- kysely/db.ts
+++ kysely/db.ts
@@ -1,4 +1,5 @@
 import { Kysely, PostgresDialect } from "kysely";
+import { Pool } from "@neondatabase/serverless";
 import { DB } from "./db-schema";
 import pg from "pg";
 
@@ -6,8 +7,19 @@ export const pool = new pg.Pool({
   connectionString: process.env.KYSELY_URL,
 });
 
-export const dbk = new Kysely<DB>({
-  dialect: new PostgresDialect({
-    pool: pool,
-  }),
+export const neonPool = new Pool({
+  connectionString: process.env.KYSELY_URL,
 });
+
+export const dbk =
+  process.env.NODE_ENV == "production"
+    ? new Kysely<DB>({
+        dialect: new PostgresDialect({
+          pool: neonPool,
+        }),
+      })
+    : new Kysely<DB>({
+        dialect: new PostgresDialect({
+          pool: pool,
+        }),
+      });
